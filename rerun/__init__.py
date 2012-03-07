4d4830ab4d226cf283a0e3b1ed6c1665ca453840
Jonathan Hartley <tartley@tartley.com>
2012-03-07 11:59:03 -0800

    re-commit of changes introduced by dropbox bodge?

--- rerun/__init__.py
+++ rerun/__init__.py
@@ -4,3 +4,6 @@ files are modified in the current directory or its subdirectories.
 '''
 from .version import __version__
 
+# __doc__ and __version__ from here are seen by pydoc, read by setup.py,
+# and generate output for --help.
+
